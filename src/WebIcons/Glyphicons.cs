4c0b51c827a21f46c50d7cf3db1f8e98d59ca800
steve.hughes <steve.hughes@creative-technologies.co.uk>
2016-11-25 17:16:18 +0000

    Added Glyphicons

---
+++ src/WebIcons/Glyphicons.cs
@@ -0,0 +1,531 @@
+﻿namespace WebIcons
+{
+    public static class Glyphicons
+    {
+        public const string Adjust = "glyphicon glyphicon-adjust";
+
+        public const string Alert = "glyphicon glyphicon-alert";
+
+        public const string AlignCenter = "glyphicon glyphicon-align-center";
+
+        public const string AlignJustify = "glyphicon glyphicon-align-justify";
+
+        public const string AlignLeft = "glyphicon glyphicon-align-left";
+
+        public const string AlignRight = "glyphicon glyphicon-align-right";
+
+        public const string Apple = "glyphicon glyphicon-apple";
+
+        public const string ArrowDown = "glyphicon glyphicon-arrow-down";
+
+        public const string ArrowLeft = "glyphicon glyphicon-arrow-left";
+
+        public const string ArrowRight = "glyphicon glyphicon-arrow-right";
+
+        public const string ArrowUp = "glyphicon glyphicon-arrow-up";
+
+        public const string Asterisk = "glyphicon glyphicon-asterisk";
+
+        public const string BabyFormula = "glyphicon glyphicon-baby-formula";
+
+        public const string Backward = "glyphicon glyphicon-backward";
+
+        public const string BanCircle = "glyphicon glyphicon-ban-circle";
+
+        public const string Barcode = "glyphicon glyphicon-barcode";
+
+        public const string Bed = "glyphicon glyphicon-bed";
+
+        public const string Bell = "glyphicon glyphicon-bell";
+
+        public const string Bishop = "glyphicon glyphicon-bishop";
+
+        public const string Bitcoin = "glyphicon glyphicon-bitcoin";
+
+        public const string Blackboard = "glyphicon glyphicon-blackboard";
+
+        public const string Bold = "glyphicon glyphicon-bold";
+
+        public const string Book = "glyphicon glyphicon-book";
+
+        public const string Bookmark = "glyphicon glyphicon-bookmark";
+
+        public const string Briefcase = "glyphicon glyphicon-briefcase";
+
+        public const string Btc = "glyphicon glyphicon-btc";
+
+        public const string Bullhorn = "glyphicon glyphicon-bullhorn";
+
+        public const string Calendar = "glyphicon glyphicon-calendar";
+
+        public const string Camera = "glyphicon glyphicon-camera";
+
+        public const string Cd = "glyphicon glyphicon-cd";
+
+        public const string Certificate = "glyphicon glyphicon-certificate";
+
+        public const string Check = "glyphicon glyphicon-check";
+
+        public const string ChevronDown = "glyphicon glyphicon-chevron-down";
+
+        public const string ChevronLeft = "glyphicon glyphicon-chevron-left";
+
+        public const string ChevronRight = "glyphicon glyphicon-chevron-right";
+
+        public const string ChevronUp = "glyphicon glyphicon-chevron-up";
+
+        public const string CircleArrowDown = "glyphicon glyphicon-circle-arrow-down";
+
+        public const string CircleArrowLeft = "glyphicon glyphicon-circle-arrow-left";
+
+        public const string CircleArrowRight = "glyphicon glyphicon-circle-arrow-right";
+
+        public const string CircleArrowUp = "glyphicon glyphicon-circle-arrow-up";
+
+        public const string Cloud = "glyphicon glyphicon-cloud";
+
+        public const string CloudDownload = "glyphicon glyphicon-cloud-download";
+
+        public const string CloudUpload = "glyphicon glyphicon-cloud-upload";
+
+        public const string Cog = "glyphicon glyphicon-cog";
+
+        public const string CollapseDown = "glyphicon glyphicon-collapse-down";
+
+        public const string CollapseUp = "glyphicon glyphicon-collapse-up";
+
+        public const string Comment = "glyphicon glyphicon-comment";
+
+        public const string Compressed = "glyphicon glyphicon-compressed";
+
+        public const string Console = "glyphicon glyphicon-console";
+
+        public const string Copy = "glyphicon glyphicon-copy";
+
+        public const string CopyrightMark = "glyphicon glyphicon-copyright-mark";
+
+        public const string CreditCard = "glyphicon glyphicon-credit-card";
+
+        public const string Cutlery = "glyphicon glyphicon-cutlery";
+
+        public const string Dashboard = "glyphicon glyphicon-dashboard";
+
+        public const string Download = "glyphicon glyphicon-download";
+
+        public const string DownloadAlt = "glyphicon glyphicon-download-alt";
+
+        public const string Duplicate = "glyphicon glyphicon-duplicate";
+
+        public const string Earphone = "glyphicon glyphicon-earphone";
+
+        public const string Edit = "glyphicon glyphicon-edit";
+
+        public const string Education = "glyphicon glyphicon-education";
+
+        public const string Eject = "glyphicon glyphicon-eject";
+
+        public const string Envelope = "glyphicon glyphicon-envelope";
+
+        public const string Equalizer = "glyphicon glyphicon-equalizer";
+
+        public const string Erase = "glyphicon glyphicon-erase";
+
+        public const string Eur = "glyphicon glyphicon-eur";
+
+        public const string Euro = "glyphicon glyphicon-euro";
+
+        public const string ExclamationSign = "glyphicon glyphicon-exclamation-sign";
+
+        public const string Expand = "glyphicon glyphicon-expand";
+
+        public const string Export = "glyphicon glyphicon-export";
+
+        public const string EyeClose = "glyphicon glyphicon-eye-close";
+
+        public const string EyeOpen = "glyphicon glyphicon-eye-open";
+
+        public const string FacetimeVideo = "glyphicon glyphicon-facetime-video";
+
+        public const string FastBackward = "glyphicon glyphicon-fast-backward";
+
+        public const string FastForward = "glyphicon glyphicon-fast-forward";
+
+        public const string File = "glyphicon glyphicon-file";
+
+        public const string Film = "glyphicon glyphicon-film";
+
+        public const string Filter = "glyphicon glyphicon-filter";
+
+        public const string Fire = "glyphicon glyphicon-fire";
+
+        public const string Flag = "glyphicon glyphicon-flag";
+
+        public const string Flash = "glyphicon glyphicon-flash";
+
+        public const string FloppyDisk = "glyphicon glyphicon-floppy-disk";
+
+        public const string FloppyOpen = "glyphicon glyphicon-floppy-open";
+
+        public const string FloppyRemove = "glyphicon glyphicon-floppy-remove";
+
+        public const string FloppySave = "glyphicon glyphicon-floppy-save";
+
+        public const string FloppySaved = "glyphicon glyphicon-floppy-saved";
+
+        public const string FolderClose = "glyphicon glyphicon-folder-close";
+
+        public const string FolderOpen = "glyphicon glyphicon-folder-open";
+
+        public const string Font = "glyphicon glyphicon-font";
+
+        public const string Forward = "glyphicon glyphicon-forward";
+
+        public const string Fullscreen = "glyphicon glyphicon-fullscreen";
+
+        public const string Gbp = "glyphicon glyphicon-gbp";
+
+        public const string Gift = "glyphicon glyphicon-gift";
+
+        public const string Glass = "glyphicon glyphicon-glass";
+
+        public const string Globe = "glyphicon glyphicon-globe";
+
+        public const string Grain = "glyphicon glyphicon-grain";
+
+        public const string HandDown = "glyphicon glyphicon-hand-down";
+
+        public const string HandLeft = "glyphicon glyphicon-hand-left";
+
+        public const string HandRight = "glyphicon glyphicon-hand-right";
+
+        public const string HandUp = "glyphicon glyphicon-hand-up";
+
+        public const string Hdd = "glyphicon glyphicon-hdd";
+
+        public const string HdVideo = "glyphicon glyphicon-hd-video";
+
+        public const string Header = "glyphicon glyphicon-header";
+
+        public const string Headphones = "glyphicon glyphicon-headphones";
+
+        public const string Heart = "glyphicon glyphicon-heart";
+
+        public const string HeartEmpty = "glyphicon glyphicon-heart-empty";
+
+        public const string Home = "glyphicon glyphicon-home";
+
+        public const string Hourglass = "glyphicon glyphicon-hourglass";
+
+        public const string IceLolly = "glyphicon glyphicon-ice-lolly";
+
+        public const string IceLollyTasted = "glyphicon glyphicon-ice-lolly-tasted";
+
+        public const string Import = "glyphicon glyphicon-import";
+
+        public const string Inbox = "glyphicon glyphicon-inbox";
+
+        public const string IndentLeft = "glyphicon glyphicon-indent-left";
+
+        public const string IndentRight = "glyphicon glyphicon-indent-right";
+
+        public const string InfoSign = "glyphicon glyphicon-info-sign";
+
+        public const string Italic = "glyphicon glyphicon-italic";
+
+        public const string Jpy = "glyphicon glyphicon-jpy";
+
+        public const string King = "glyphicon glyphicon-king";
+
+        public const string Knight = "glyphicon glyphicon-knight";
+
+        public const string Lamp = "glyphicon glyphicon-lamp";
+
+        public const string Leaf = "glyphicon glyphicon-leaf";
+
+        public const string LevelUp = "glyphicon glyphicon-level-up";
+
+        public const string Link = "glyphicon glyphicon-link";
+
+        public const string List = "glyphicon glyphicon-list";
+
+        public const string ListAlt = "glyphicon glyphicon-list-alt";
+
+        public const string Lock = "glyphicon glyphicon-lock";
+
+        public const string LogIn = "glyphicon glyphicon-log-in";
+
+        public const string LogOut = "glyphicon glyphicon-log-out";
+
+        public const string Magnet = "glyphicon glyphicon-magnet";
+
+        public const string MapMarker = "glyphicon glyphicon-map-marker";
+
+        public const string MenuDown = "glyphicon glyphicon-menu-down";
+
+        public const string MenuHamburger = "glyphicon glyphicon-menu-hamburger";
+
+        public const string MenuLeft = "glyphicon glyphicon-menu-left";
+
+        public const string MenuRight = "glyphicon glyphicon-menu-right";
+
+        public const string MenuUp = "glyphicon glyphicon-menu-up";
+
+        public const string Minus = "glyphicon glyphicon-minus";
+
+        public const string MinusSign = "glyphicon glyphicon-minus-sign";
+
+        public const string ModalWindow = "glyphicon glyphicon-modal-window";
+
+        public const string Move = "glyphicon glyphicon-move";
+
+        public const string Music = "glyphicon glyphicon-music";
+
+        public const string NewWindow = "glyphicon glyphicon-new-window";
+
+        public const string ObjectAlignBottom = "glyphicon glyphicon-object-align-bottom";
+
+        public const string ObjectAlignHorizontal = "glyphicon glyphicon-object-align-horizontal";
+
+        public const string ObjectAlignLeft = "glyphicon glyphicon-object-align-left";
+
+        public const string ObjectAlignRight = "glyphicon glyphicon-object-align-right";
+
+        public const string ObjectAlignTop = "glyphicon glyphicon-object-align-top";
+
+        public const string ObjectAlignVertical = "glyphicon glyphicon-object-align-vertical";
+
+        public const string Off = "glyphicon glyphicon-off";
+
+        public const string Oil = "glyphicon glyphicon-oil";
+
+        public const string Ok = "glyphicon glyphicon-ok";
+
+        public const string OkCircle = "glyphicon glyphicon-ok-circle";
+
+        public const string OkSign = "glyphicon glyphicon-ok-sign";
+
+        public const string Open = "glyphicon glyphicon-open";
+
+        public const string OpenFile = "glyphicon glyphicon-open-file";
+
+        public const string OptionHorizontal = "glyphicon glyphicon-option-horizontal";
+
+        public const string OptionVertical = "glyphicon glyphicon-option-vertical";
+
+        public const string Paperclip = "glyphicon glyphicon-paperclip";
+
+        public const string Paste = "glyphicon glyphicon-paste";
+
+        public const string Pause = "glyphicon glyphicon-pause";
+
+        public const string Pawn = "glyphicon glyphicon-pawn";
+
+        public const string Pencil = "glyphicon glyphicon-pencil";
+
+        public const string Phone = "glyphicon glyphicon-phone";
+
+        public const string PhoneAlt = "glyphicon glyphicon-phone-alt";
+
+        public const string Picture = "glyphicon glyphicon-picture";
+
+        public const string PiggyBank = "glyphicon glyphicon-piggy-bank";
+
+        public const string Plane = "glyphicon glyphicon-plane";
+
+        public const string Play = "glyphicon glyphicon-play";
+
+        public const string PlayCircle = "glyphicon glyphicon-play-circle";
+
+        public const string Plus = "glyphicon glyphicon-plus";
+
+        public const string PlusSign = "glyphicon glyphicon-plus-sign";
+
+        public const string Print = "glyphicon glyphicon-print";
+
+        public const string Pushpin = "glyphicon glyphicon-pushpin";
+
+        public const string Qrcode = "glyphicon glyphicon-qrcode";
+
+        public const string Queen = "glyphicon glyphicon-queen";
+
+        public const string QuestionSign = "glyphicon glyphicon-question-sign";
+
+        public const string Random = "glyphicon glyphicon-random";
+
+        public const string Record = "glyphicon glyphicon-record";
+
+        public const string Refresh = "glyphicon glyphicon-refresh";
+
+        public const string RegistrationMark = "glyphicon glyphicon-registration-mark";
+
+        public const string Remove = "glyphicon glyphicon-remove";
+
+        public const string RemoveCircle = "glyphicon glyphicon-remove-circle";
+
+        public const string RemoveSign = "glyphicon glyphicon-remove-sign";
+
+        public const string Repeat = "glyphicon glyphicon-repeat";
+
+        public const string ResizeFull = "glyphicon glyphicon-resize-full";
+
+        public const string ResizeHorizontal = "glyphicon glyphicon-resize-horizontal";
+
+        public const string ResizeSmall = "glyphicon glyphicon-resize-small";
+
+        public const string ResizeVertical = "glyphicon glyphicon-resize-vertical";
+
+        public const string Retweet = "glyphicon glyphicon-retweet";
+
+        public const string Road = "glyphicon glyphicon-road";
+
+        public const string Rub = "glyphicon glyphicon-rub";
+
+        public const string Ruble = "glyphicon glyphicon-ruble";
+
+        public const string Save = "glyphicon glyphicon-save";
+
+        public const string Saved = "glyphicon glyphicon-saved";
+
+        public const string SaveFile = "glyphicon glyphicon-save-file";
+
+        public const string Scale = "glyphicon glyphicon-scale";
+
+        public const string Scissors = "glyphicon glyphicon-scissors";
+
+        public const string Screenshot = "glyphicon glyphicon-screenshot";
+
+        public const string SdVideo = "glyphicon glyphicon-sd-video";
+
+        public const string Search = "glyphicon glyphicon-search";
+
+        public const string Send = "glyphicon glyphicon-send";
+
+        public const string Share = "glyphicon glyphicon-share";
+
+        public const string ShareAlt = "glyphicon glyphicon-share-alt";
+
+        public const string ShoppingCart = "glyphicon glyphicon-shopping-cart";
+
+        public const string Signal = "glyphicon glyphicon-signal";
+
+        public const string Sort = "glyphicon glyphicon-sort";
+
+        public const string SortByAlphabet = "glyphicon glyphicon-sort-by-alphabet";
+
+        public const string SortByAlphabetAlt = "glyphicon glyphicon-sort-by-alphabet-alt";
+
+        public const string SortByAttributes = "glyphicon glyphicon-sort-by-attributes";
+
+        public const string SortByAttributesAlt = "glyphicon glyphicon-sort-by-attributes-alt";
+
+        public const string SortByOrder = "glyphicon glyphicon-sort-by-order";
+
+        public const string SortByOrderAlt = "glyphicon glyphicon-sort-by-order-alt";
+
+        public const string Sound51 = "glyphicon glyphicon-sound-5-1";
+
+        public const string Sound61 = "glyphicon glyphicon-sound-6-1";
+
+        public const string Sound71 = "glyphicon glyphicon-sound-7-1";
+
+        public const string SoundDolby = "glyphicon glyphicon-sound-dolby";
+
+        public const string SoundStereo = "glyphicon glyphicon-sound-stereo";
+
+        public const string Star = "glyphicon glyphicon-star";
+
+        public const string StarEmpty = "glyphicon glyphicon-star-empty";
+
+        public const string Stats = "glyphicon glyphicon-stats";
+
+        public const string StepBackward = "glyphicon glyphicon-step-backward";
+
+        public const string StepForward = "glyphicon glyphicon-step-forward";
+
+        public const string Stop = "glyphicon glyphicon-stop";
+
+        public const string Subscript = "glyphicon glyphicon-subscript";
+
+        public const string Subtitles = "glyphicon glyphicon-subtitles";
+
+        public const string Sunglasses = "glyphicon glyphicon-sunglasses";
+
+        public const string Superscript = "glyphicon glyphicon-superscript";
+
+        public const string Tag = "glyphicon glyphicon-tag";
+
+        public const string Tags = "glyphicon glyphicon-tags";
+
+        public const string Tasks = "glyphicon glyphicon-tasks";
+
+        public const string Tent = "glyphicon glyphicon-tent";
+
+        public const string TextBackground = "glyphicon glyphicon-text-background";
+
+        public const string TextColor = "glyphicon glyphicon-text-color";
+
+        public const string TextHeight = "glyphicon glyphicon-text-height";
+
+        public const string TextSize = "glyphicon glyphicon-text-size";
+
+        public const string TextWidth = "glyphicon glyphicon-text-width";
+
+        public const string Th = "glyphicon glyphicon-th";
+
+        public const string ThLarge = "glyphicon glyphicon-th-large";
+
+        public const string ThList = "glyphicon glyphicon-th-list";
+
+        public const string ThumbsDown = "glyphicon glyphicon-thumbs-down";
+
+        public const string ThumbsUp = "glyphicon glyphicon-thumbs-up";
+
+        public const string Time = "glyphicon glyphicon-time";
+
+        public const string Tint = "glyphicon glyphicon-tint";
+
+        public const string Tower = "glyphicon glyphicon-tower";
+
+        public const string Transfer = "glyphicon glyphicon-transfer";
+
+        public const string Trash = "glyphicon glyphicon-trash";
+
+        public const string TreeConifer = "glyphicon glyphicon-tree-conifer";
+
+        public const string TreeDeciduous = "glyphicon glyphicon-tree-deciduous";
+
+        public const string TriangleBottom = "glyphicon glyphicon-triangle-bottom";
+
+        public const string TriangleLeft = "glyphicon glyphicon-triangle-left";
+
+        public const string TriangleRight = "glyphicon glyphicon-triangle-right";
+
+        public const string TriangleTop = "glyphicon glyphicon-triangle-top";
+
+        public const string Unchecked = "glyphicon glyphicon-unchecked";
+
+        public const string Upload = "glyphicon glyphicon-upload";
+
+        public const string Usd = "glyphicon glyphicon-usd";
+
+        public const string User = "glyphicon glyphicon-user";
+
+        public const string VolumeDown = "glyphicon glyphicon-volume-down";
+
+        public const string VolumeOff = "glyphicon glyphicon-volume-off";
+
+        public const string VolumeUp = "glyphicon glyphicon-volume-up";
+
+        public const string WarningSign = "glyphicon glyphicon-warning-sign";
+
+        public const string Wrench = "glyphicon glyphicon-wrench";
+
+        public const string Xbt = "glyphicon glyphicon-xbt";
+
+        public const string Yen = "glyphicon glyphicon-yen";
+
+        public const string ZoomIn = "glyphicon glyphicon-zoom-in";
+
+        public const string ZoomOut = "glyphicon glyphicon-zoom-out";
+    }
+}
